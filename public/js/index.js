8d5d197140686988896c75a8ba6aea036650f4b7
real0131 <real0131@gmail.com>
2017-05-07 18:14:42 +0900

    add cover-content hide show

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,29 @@
 var btn_show_cover_content = document.getElementById('btn-show-cover-content');
+var carousel_content_main = document.getElementById('carousel-content-main');
 
 function onClick(id) {
     switch(id){
         case "show-cover-content":
             var pattern = /animation?/g;
             if (!pattern.test(btn_show_cover_content.className.toString())){
+                removeClass(carousel_content_main,'show');
                 addClass(btn_show_cover_content,'animation');
+                addClass(carousel_content_main,'hide-anim');
+                carousel_content_main.addEventListener('animationend',
+                function () { addClass(carousel_content_main,'hide'); });
             }else{
                 removeClass(btn_show_cover_content,'animation');
+                removeClass(carousel_content_main,'hide-anim');
+                removeClass(carousel_content_main,'hide');
                 addClass(btn_show_cover_content,'ToDefault');
                 btn_show_cover_content.addEventListener('animationend',
-                    function(){removeClass(btn_show_cover_content,'ToDefault');});
+                    function () { removeClass(btn_show_cover_content,'ToDefault'); });
+                addClass(carousel_content_main,'show-anim');
+                addClass(carousel_content_main,'show');
+                carousel_content_main.addEventListener('animationend',
+                    function () {
+                        removeClass(carousel_content_main,'show-anim');
+                });
             }
             break;
     }
